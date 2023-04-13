48e02afb52b874ad7774017bec41fc207b1e0d0c
0xpeche <114630968+0xpeche@users.noreply.github.com>
2023-04-13 16:24:26 +0200

    cleanup

--- test/StyxPermitProxy.t.ts
+++ test/StyxPermitProxy.t.ts
@@ -21,7 +21,9 @@ const tokenOutAddress = "0x514910771af9ca656af840dff83e8264ecf986ca"; // LINK
 const ETH = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 const SIG_DEADLINE = "115792089237316195423570985008687907853269984665640564039457584007913129639935";
 
-const ZeroExchangeProxyArbitrum = "0xdef1c0ded9bec7f1a1670819833240f027b25eff"; // we use mainnet because arb forking is wonky
+const ZeroExchangeProxy = "0xdef1c0ded9bec7f1a1670819833240f027b25eff"; // we use mainnet because arb forking is wonky
+const FireBirdRouter = "0xe0C38b2a8D09aAD53f1C67734B9A95E43d5981c0";
+const OneInchRouter = "0x1111111254fb6c44bac0bed2854e76f90643097d";
 
 const testAmountIn = ethers.utils.parseEther("100");
 
@@ -62,7 +64,9 @@ describe("Styx Permit2 Proxy", function () {
         const tokenOut = new ethers.Contract(tokenOutAddress, MINIMAL_ERC20_ABI, addr1)
 
         // Setup routers
-        await ownerHelper.setAggregator(permitProxy.address, ZeroExchangeProxyArbitrum, "0")
+        await ownerHelper.setAggregator(permitProxy.address, ZeroExchangeProxy, "0")
+        await ownerHelper.setAggregator(permitProxy.address, OneInchRouter, "1")
+        await ownerHelper.setAggregator(permitProxy.address, FireBirdRouter, "2")
 
         // Give unlimited approval to Permit2
         await tokenIn.connect(addr1).approve(PERMIT2_ADDRESS, constants.MaxInt256)
@@ -229,5 +233,167 @@ describe("Styx Permit2 Proxy", function () {
         //console.log(ethers.utils.formatEther(balEthAfter.toString()), "balance Eth After")
 
     })
+
+    // it("Should execute a token to token trade using 0x Exchange", async function () {
+    //     const { tokenIn, tokenOut, arbAddressTable, utilsPermit, permitProxy, addr1 } = await loadFixture(fixtures);
+
+    //     const amountInCint = await utilsPermit.compress(testAmountIn);
+    //     // We have to use the uncompressed number for our quote and for the permit
+    //     const amountInUncompressed = await utilsPermit.uncompress(amountInCint);
+
+    //     const quoteResponse = await fetch(
+    //         `https://api.0x.org/swap/v1/quote?buyToken=${tokenOut.address}&sellAmount=${amountInUncompressed}&sellToken=${tokenIn.address}`
+    //     );
+
+    //     await expect(quoteResponse.status).to.be.equal(200);
+
+    //     const quote = await quoteResponse.json();
+
+    //     // Register token Index from Arb Address Table
+    //     await arbAddressTable.register(tokenIn.address);
+    //     await arbAddressTable.register(tokenOut.address);
+
+    //     const tokenInIndex = await arbAddressTable.lookup(tokenIn.address)
+    //     const tokenOutIndex = await arbAddressTable.lookup(tokenOut.address)
+
+    //     const currNonce = await ethers.provider.getStorageAt(permitProxy.address, ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(["address", "uint256"], [addr1.address, "0x0"])))
+
+    //     const permit: PermitTransferFrom = {
+    //         permitted: {
+    //             token: tokenIn.address,
+    //             amount: amountInUncompressed
+    //         },
+    //         spender: permitProxy.address,
+    //         nonce: parseInt(currNonce) + 1337 + 420 + 69 + 15537393,
+    //         deadline: SIG_DEADLINE
+    //     };
+
+    //     const witness: Witness = {
+    //         witnessTypeName: "Witness",
+    //         witnessType: { Witness: [{ name: "swapCalldata", type: "bytes32" }] },
+    //         witness: { swapCalldata: ethers.utils.keccak256(quote.data) }
+    //     }
+
+    //     const { chainId } = await ethers.provider.getNetwork();
+
+    //     const { domain, types, values } = SignatureTransfer.getPermitData(permit, PERMIT2_ADDRESS, chainId, witness);
+
+    //     const signature = await addr1._signTypedData(domain, types, values);
+
+    //     const { r, s, v } = ethers.utils.splitSignature(signature);
+
+    //     const [rCompact, vsCompact] = await utilsPermit.getCompactSignature(v, r, s);
+
+    //     const packedData = ethers.BigNumber.from("0")
+    //         .shl(112)
+    //         .or(ethers.BigNumber.from(amountInCint).shl(48))
+    //         .or(ethers.BigNumber.from(tokenInIndex).shl(24))
+    //         .or(ethers.BigNumber.from(tokenOutIndex));
+
+    //     const encoded = ethers.utils.concat([
+    //         ethers.utils.defaultAbiCoder.encode(["uint120"], [packedData]),
+    //         ethers.utils.defaultAbiCoder.encode(["bytes32"], [rCompact]),
+    //         ethers.utils.defaultAbiCoder.encode(["bytes32"], [vsCompact]),
+    //         quote.data
+    //     ]);
+
+    //     const rawTx = {
+    //         to: permitProxy.address,
+    //         data: encoded,
+    //         value: quote.value
+    //     }
+
+    //     //const balTokenOutBefore = await tokenOut.balanceOf(addr1.address)
+
+    //     const signedTx = await addr1.sendTransaction(rawTx)
+
+    //     //const balTokenOutAfter = await tokenOut.balanceOf(addr1.address)
+
+    //     //console.log(ethers.utils.formatEther(balTokenOutBefore.toString()), "balance tokenOut Before")
+    //     //console.log(ethers.utils.formatEther(balTokenOutAfter.toString()), "balance tokenOut After")
+
+    // })
+
+    // it("Should execute a token to token trade using 1inch", async function () {
+    //     const { tokenIn, tokenOut, arbAddressTable, utilsPermit, permitProxy, addr1 } = await loadFixture(fixtures);
+
+    //     const amountInCint = await utilsPermit.compress(testAmountIn);
+    //     // We have to use the uncompressed number for our quote and for the permit
+    //     const amountInUncompressed = await utilsPermit.uncompress(amountInCint);
+
+    //     const quoteResponse = await fetch(
+    //         `https://api.1inch.io/v4.0/1/swap?fromTokenAddress=${tokenIn.address}&toTokenAddress=${tokenOut.address}&amount=${amountInUncompressed}&slippage=${0.5}&disableEstimate=true&fromAddress=${permitProxy.address}`
+    //     );
+
+    //     await expect(quoteResponse.status).to.be.equal(200);
+
+    //     const quote = await quoteResponse.json();
+
+    //     console.log(quote)
+
+    //     // Register token Index from Arb Address Table
+    //     await arbAddressTable.register(tokenIn.address);
+    //     await arbAddressTable.register(tokenOut.address);
+
+    //     const tokenInIndex = await arbAddressTable.lookup(tokenIn.address)
+    //     const tokenOutIndex = await arbAddressTable.lookup(tokenOut.address)
+
+    //     const currNonce = await ethers.provider.getStorageAt(permitProxy.address, ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(["address", "uint256"], [addr1.address, "0x0"])))
+
+    //     const permit: PermitTransferFrom = {
+    //         permitted: {
+    //             token: tokenIn.address,
+    //             amount: amountInUncompressed
+    //         },
+    //         spender: permitProxy.address,
+    //         nonce: parseInt(currNonce) + 1337 + 420 + 69 + 15537393,
+    //         deadline: SIG_DEADLINE
+    //     };
+
+    //     const witness: Witness = {
+    //         witnessTypeName: "Witness",
+    //         witnessType: { Witness: [{ name: "swapCalldata", type: "bytes32" }] },
+    //         witness: { swapCalldata: ethers.utils.keccak256(quote.tx.data) }
+    //     }
+
+    //     const { chainId } = await ethers.provider.getNetwork();
+
+    //     const { domain, types, values } = SignatureTransfer.getPermitData(permit, PERMIT2_ADDRESS, chainId, witness);
+
+    //     const signature = await addr1._signTypedData(domain, types, values);
+
+    //     const { r, s, v } = ethers.utils.splitSignature(signature);
+
+    //     const [rCompact, vsCompact] = await utilsPermit.getCompactSignature(v, r, s);
+
+    //     const packedData = ethers.BigNumber.from("1") // routerId
+    //         .shl(112)
+    //         .or(ethers.BigNumber.from(amountInCint).shl(48))
+    //         .or(ethers.BigNumber.from(tokenInIndex).shl(24))
+    //         .or(ethers.BigNumber.from(tokenOutIndex));
+
+    //     const encoded = ethers.utils.concat([
+    //         ethers.utils.defaultAbiCoder.encode(["uint120"], [packedData]),
+    //         ethers.utils.defaultAbiCoder.encode(["bytes32"], [rCompact]),
+    //         ethers.utils.defaultAbiCoder.encode(["bytes32"], [vsCompact]),
+    //         quote.tx.data
+    //     ]);
+
+    //     const rawTx = {
+    //         to: permitProxy.address,
+    //         data: encoded,
+    //         value: quote.tx.value
+    //     }
+
+    //     //const balTokenOutBefore = await tokenOut.balanceOf(addr1.address)
+
+    //     const signedTx = await addr1.sendTransaction(rawTx)
+
+    //     //const balTokenOutAfter = await tokenOut.balanceOf(addr1.address)
+
+    //     //console.log(ethers.utils.formatEther(balTokenOutBefore.toString()), "balance tokenOut Before")
+    //     //console.log(ethers.utils.formatEther(balTokenOutAfter.toString()), "balance tokenOut After")
+
+    // })
 
 })
